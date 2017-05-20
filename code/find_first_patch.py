8186a7af6493e27a57a82193c4ec1bd30e364a10
yc3240 <yc3240@columbia.edu>
2017-05-20 10:45:04 -0400

    Paramatize find_first_patch.py

--- code/find_first_patch.py
+++ code/find_first_patch.py
@@ -19,46 +19,49 @@ Functions:
 import argparse
 import cv2
 
+global fp_offset1, fp_offset2, fp_image
+
 # the signature is required by cv2.setMouseCallback()
 def click(event, x, y, flags, param):
-    global offset1, offset2
+    global fp_offset1, fp_offset2
 
     if event == cv2.EVENT_LBUTTONUP:
-        start = (x-offset1, y-offset1)
-        end = (x+offset2,y+offset2)
-        print 'bounding box', start, ' -> ', end
+        start = (x-fp_offset1, y-fp_offset1)
+        end = (x+fp_offset2,y+fp_offset2)
+        # coordinates in cv2 is reversed, so print out reversely
+        print 'bounding box', start[::-1], ' -> ', end[::-1]
 
         # draw a rectangle around the region of interest
-        #image = original_image.copy()
-        cv2.rectangle(image, start, end, (255,0,0),2 )
-        cv2.imshow("image", image)
+        cv2.rectangle(fp_image, start, end, (255,0,0),2 )
+        cv2.imshow("image", fp_image)
+
+#
+def semi_auto_recognize(image_path, patch_size):
+    global fp_offset1, fp_offset2, fp_image
+    fp_image = cv2.imread(args["image"])   # load image
+    original_image = fp_image.copy()
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", click)
+
+    fp_offset1 = fp_offset2 = patch_size/2    # compute corner coordinate
+    if patch_size%2 == 1:               # handle odd number
+        fp_offset2 += 1
+
+    while True:
+        cv2.imshow("image",fp_image)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("c"):
+            fp_image = original_image.copy()
+
+        elif key == ord("q"):
+            break
+    cv2.destroyAllWindows()
+
 
 # construct the argumet parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Image path")
 ap.add_argument("-s", "--size", required=False, help="Size of bounding box", type=int, default=64)
 args = vars(ap.parse_args())
-
-# load image
-image = cv2.imread(args["image"])
-original_image = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click)
-
-patch_size = int(args["size"])
-offset1 = offset2 = patch_size/2
-if patch_size%2 == 1:
-    offset2 += 1
-
-while True:
-    #display image
-    cv2.imshow("image",image)
-    key = cv2.waitKey(1) & 0xFF
-
-    if key == ord("r"):
-        image = original_image.copy()
-
-    elif key == ord("c"):
-        break
-
-cv2.destroyAllWindows()
+semi_auto_recognize(args["image"], args["size"])
